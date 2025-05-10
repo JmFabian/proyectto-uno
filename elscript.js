8c95f068db7973128915cf12ba62b9689e696bb2
JmFabian <72355352@certus.edu.pe>
2025-05-09 22:11:02 -0500

    ver

--- elscript.js
+++ elscript.js
@@ -58,4 +58,50 @@ function FAgregarTextoPermanente(){
     console.log(vTexto1); 
 }
 
+function FUsoFor1(){
+    const vTextoClass = document.getElementsByClassName("classFOR1");
+    var vTextoTotal="";
+    // como length=4 --> ira desde 0 hasta 3
+    for(let i=0 ; i< vTextoClass.length ; i++ )
+    {
+      vTextoClass[i].style.border ="2px solid red" ;
+      vTextoTotal=vTextoTotal + vTextoClass[i].innerText+'-';
+  
+    } // fin de for
+    var nuevoH1 =document.createElement("h1");
+    var texto = document.createTextNode(vTextoTotal);
+    nuevoH1.appendChild(texto);
+    vTextoClass[3].appendChild(nuevoH1);
+    
+    const miBoton = document.getElementById('miBoton'); // Reemplaza 'miBoton' con el ID de tu botón
+  
+      console.log("¡La función del botón se ejecutó!");
+      // Aquí va el código de tu función
+    
+      // Deshabilitar el botón después de la primera ejecución
+      miBoton.disabled = true;
+      miBoton.hidden = true;
+      miBoton.textContent = 'Ya se ejecutó'; // Opcional: cambiar el texto del botón
+  
+  
+  }  // fin de function
+  
+  function FEliminarConcat()
+  {
+    const vTextoClass = document.getElementsByClassName("classFOR1");
+    let rpta =prompt("Estas seguro de eliminar el texto concatenado?(S/N)",'');
+    if (rpta=="S" || rpta=="s")  //  doble || significa o
+      { vTextoClass[3].remove();  // eliminar la class de posicion [3]
+        alert("CONCATENCION ELMINADA");
+      }  
+    else
+      { if(rpta=="N" || rpta=="n")
+        { alert("NO SE elimnará");
+        }
+        else
+        { alert("OPCION INCORRECTA-INgresa nuevamente");
+        }
+      }
+    //removeChild funciona cuando un nodo(class, id, etx) esta dentro de otro
+  }
     
